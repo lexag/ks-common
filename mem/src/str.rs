114994799b2b72d4c485b66787edaa202d674bad
Lexag <lexa.grebnets@gmail.com>
2026-01-11 14:13:09 +0100

    StaticString from_int(i32)

--- mem/src/str.rs
+++ mem/src/str.rs
@@ -27,6 +27,49 @@ impl<const L: usize> StaticString<L> {
         a
     }
 
+    fn len_of_int(mut val: i32) -> usize {
+        let neg_sign = val < 0;
+        val = val.abs();
+        let mut num_digits = 1;
+        let mut comp = 10;
+        while comp - 1 < val {
+            comp *= 10;
+            num_digits += 1;
+        }
+        if neg_sign {
+            num_digits += 1;
+        }
+        assert_ne!(num_digits, 0);
+        num_digits
+    }
+
+    pub fn from_int(mut val: i32) -> Self {
+        let digits = Self::len_of_int(val.abs());
+        let neg = val < 0;
+        let offs = if neg { 1 } else { 0 };
+        let mut s = Self::empty();
+        val = val.abs();
+        if neg {
+            s.set_char(0, b'-');
+        }
+        if digits + offs > L && L <= 2 + offs {
+            s = Self::new("###");
+        } else if digits + offs > L {
+            s.set_char(
+                offs,
+                0x30 + ((val / 10_i32.pow(digits as u32 - 2) + 5) / 10) as u8,
+            );
+            s.set_char(1 + offs, b'e');
+            s.set_char(2 + offs, 0x30 + digits as u8 - 1);
+        } else {
+            for i in (0..digits).rev() {
+                s.set_char(i + offs, 0x30 + (val % 10) as u8);
+                val /= 10;
+            }
+        }
+        s
+    }
+
     pub fn set_char(&mut self, idx: usize, char: u8) {
         if idx < self.content.len() {
             self.content[idx] = char;
@@ -143,5 +186,40 @@ mod tests {
         assert_eq!(b.str(), "Hello Wo");
         assert_eq!(c.str(), "Greeting");
         assert_eq!(d.str(), "");
+    }
+
+    #[test]
+    fn num_digits() {
+        assert_eq!(StaticString::<0>::len_of_int(0), 1);
+        assert_eq!(StaticString::<0>::len_of_int(1), 1);
+        assert_eq!(StaticString::<0>::len_of_int(-1), 2);
+        assert_eq!(StaticString::<0>::len_of_int(-9), 2);
+        assert_eq!(StaticString::<0>::len_of_int(10), 2);
+        assert_eq!(StaticString::<0>::len_of_int(99), 2);
+        assert_eq!(StaticString::<0>::len_of_int(100), 3);
+        assert_eq!(StaticString::<0>::len_of_int(999), 3);
+        assert_eq!(StaticString::<0>::len_of_int(1000), 4);
+        assert_eq!(StaticString::<0>::len_of_int(9999), 4);
+        assert_eq!(StaticString::<0>::len_of_int(-1234), 5);
+    }
+
+    #[test]
+    fn format_ints() {
+        assert_eq!(StaticString::<8>::from_int(0).str(), "0");
+        assert_eq!(StaticString::<8>::from_int(1).str(), "1");
+        assert_eq!(StaticString::<8>::from_int(-1).str(), "-1");
+        assert_eq!(StaticString::<8>::from_int(192).str(), "192");
+        assert_eq!(StaticString::<1>::from_int(1234).str(), "#");
+        assert_eq!(StaticString::<2>::from_int(1234).str(), "##");
+        assert_eq!(StaticString::<3>::from_int(1234).str(), "1e3");
+        assert_eq!(StaticString::<4>::from_int(1234).str(), "1234");
+        assert_eq!(StaticString::<1>::from_int(-1234).str(), "#");
+        assert_eq!(StaticString::<2>::from_int(-1234).str(), "##");
+        assert_eq!(StaticString::<3>::from_int(-1234).str(), "###");
+        assert_eq!(StaticString::<4>::from_int(-1234).str(), "-1e3");
+        assert_eq!(StaticString::<5>::from_int(-1234).str(), "-1234");
+        assert_eq!(StaticString::<8>::from_int(123456789).str(), "1e8");
+        assert_eq!(StaticString::<8>::from_int(187654321).str(), "2e8");
+        assert_eq!(StaticString::<16>::from_int(123456789).str(), "123456789");
     }
 }
