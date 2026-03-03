86a7a1a988ec3d4ae6086068680b049d774fca25
Lexag <lexa.grebnets@gmail.com>
2026-03-03 15:07:42 +0100

    fix clippy errors

--- local/src/playbackhandlerstatus.rs
+++ local/src/playbackhandlerstatus.rs
@@ -1,8 +1,10 @@
-use mem::network::SubscriberInfo;
+extern crate std;
+use std::vec::Vec;
 
 /// State of audio playback handler
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Default, Clone)]
 pub struct PlaybackHandlerStatus {
-    clips: Vec<Vec<u16>>,
+    /// List indexed by channel of list of playback clips currently loaded
+    pub clips: Vec<Vec<u16>>,
 }
