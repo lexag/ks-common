7eab203cb9f53566a13d4539c112b542914bad9f
Lexag <lexa.grebnets@gmail.com>
2026-07-29 15:36:13 +0200

    fix(ui): export selector_list_index

--- ui/src/components/mod.rs
+++ ui/src/components/mod.rs
@@ -12,6 +12,7 @@ pub(crate) const SELECTOR_LIST_MIN_ITEM_HEIGHT: f32 = 64.0;
 pub use button::Button;
 pub use button::ToggleButton;
 pub use numpad::Numpad;
+pub use selector_list::selector_list_index;
 pub use selector_list::selector_list_value;
 pub use slider::big_slider;
 pub use squarish_grid::SquarishGrid;
