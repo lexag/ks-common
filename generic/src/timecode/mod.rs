fe89f9d1efa88880cdb3043395a3a2228da6a540
Lexag <lexa.grebnets@gmail.com>
2026-07-30 14:37:48 +0200

    fix(timecode): add read_timecode_confidence to trait TimecodeReader

--- generic/src/timecode/mod.rs
+++ generic/src/timecode/mod.rs
@@ -847,6 +847,16 @@ pub trait TimecodeReader {
     /// Read the next timecode from the source
     fn read_timecode(&mut self) -> Result<Option<Timecode>, TimecodeError>;
 
+    /// Read the next timecode from the source, and also return confidence in that timecode (0.0..1.0)
+    /// as (Timecode, confidence)
+    /// By default this calls [`TimecodeReader::read_timecode`] with confidence 0.0 on None and
+    /// confidence 1.0 on Some(tc).
+    /// Overwrite this implementation for readers that reasonably can have varying confidence
+    fn read_timecode_confidence(&mut self) -> Result<(Timecode, f32), TimecodeError> {
+        self.read_timecode()?
+            .map_or_else(|| Ok((Timecode::default(), 0.0)), |t| Ok((t, 1.0)))
+    }
+
     /// Get the current frame rate
     fn frame_rate(&self) -> FrameRate;
 
